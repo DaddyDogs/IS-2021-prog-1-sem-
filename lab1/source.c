ecb1fd231d9994df60f0ab9ae455fe8ca2f4623e
Daddy dogs <ans.ermolaeva@yandex.ru>
2021-11-25 16:09:57 +0300

    feat: add changes after review

--- lab1/source.c
+++ lab1/source.c
@@ -1,41 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 int main(int argc, char *argv[]) {
-    FILE *fin;
-    fin = fopen(argv[2], "r");
-    if (argc != 3) {
+    FILE *file;
+    file = fopen(argv[2], "r");
+    if (argc != 3)
+    {
         printf("Not enough of arguments");
     }
-    else if ((strcmp(argv[1], "-c") == 0) || (strcmp(argv[1], "--bytes") == 0)) {
-        long count_bites;
-        count_bites = 0;
+    else if ((strcmp(argv[1], "-c") == 0) || (strcmp(argv[1], "--bytes") == 0))
+    {
+        long count_bytes;
+        count_bytes = 0;
         char symbole;
         symbole = fgetc(fin);
-        while (symbole != EOF) {
-            if (symbole == '\n') {
-                count_bites = count_bites + 1;
+        while (symbole != EOF)
+        {
+            if (symbole == '\n')
+            {
+                count_bytes = count_bytes + 1;
             }
-            count_bites = count_bites + 1;
+            count_bytes = count_bytes + 1;
             symbole = fgetc(fin);
         }
-        printf("%ld\n", count_bites);
-    } else if ((strcmp(argv[1], "-l") == 0) || (strcmp(argv[1], "--lines") == 0)) {
+        printf("%ld\n", count_bytes);
+    }
+    else if ((strcmp(argv[1], "-l") == 0) || (strcmp(argv[1], "--lines") == 0))
+    {
         int symbole, count_lines;
         count_lines = 0;
         symbole = 50;
-        while (symbole != EOF) {
+        while (symbole != EOF)
+        {
             symbole = fgetc(fin);
             if (symbole == '\n')
                 ++count_lines;
         }
         printf("%d\n", count_lines + 1);
-    } else if ((strcmp(argv[1], "-w") == 0) || (strcmp(argv[1], "--words") == 0)) {
+    }
+    else if ((strcmp(argv[1], "-w") == 0) || (strcmp(argv[1], "--words") == 0))
+    {
         int symbole, count_words = 0, is_word;
         symbole = 50;
-        while (symbole != EOF) {
+        while (symbole != EOF)
+        {
             symbole = fgetc(fin);
-            if (symbole == ' ' || symbole == '\n' || symbole == '\t' || symbole == EOF) {
-                if (is_word == 1) {
+            if (symbole == ' ' || symbole == '\n' || symbole == '\t' || symbole == EOF)
+            {
+                if (is_word == 1)
+                {
                     is_word = 0;
                     ++count_words;
                 }
@@ -43,9 +55,10 @@ int main(int argc, char *argv[]) {
         }
         printf("%d\n", count_words);
     }
-    else {
+    else
+    {
         printf("Invalid option");
     }
-    fclose(fin);
+    fclose(file);
     return 0;
 }
